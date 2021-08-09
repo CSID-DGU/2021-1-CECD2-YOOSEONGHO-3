aacd6e3d18fda792fc4f0f06a151eb96b44296a4
유수현 <rhan0871@naver.com>
2021-08-09 16:13:40 +0900

    복지정보 탐색 결과를 슬라이드 형태로 제공하게 변경

--- frontend/src/components/DialogContainer.js
+++ frontend/src/components/DialogContainer.js
@@ -1,52 +1,54 @@
 import { useState, useEffect, useRef } from 'react';
 import { MailIcon } from '@heroicons/react/solid'
 import MessageObj from './MessageObj';
-import {useSelector,useDispatch} from 'react-redux';
-import { sendAndReceive,recordUserMsg } from '../store/_actions/DialogAction';
+import { useSelector, useDispatch } from 'react-redux';
+import { sendAndReceive, recordUserMsg } from '../store/_actions/DialogAction';
 
 function DialogContainer() {
     const inputRef = useRef(null);
     const messagesEndRef = useRef(null);
-    const dispatch=useDispatch();
-    const dialog=useSelector(state=>state.dialog);
+    const scrollBtnRef=useRef(null);
+    const dispatch = useDispatch();
+    const dialog = useSelector(state => state.dialog);
 
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
     }
 
-    const scrollToBottom = () => { 
-        //scollIntoView 사용 시 상단 overflow문제가 발생해서 
-        messagesEndRef.current.parentNode.scrollTop=messagesEndRef.current.offsetTop;
-        
+    const scrollToBottom = () => {
+        //scollIntoView 사용 시 상단 overflow문제가 발생해서 scrollTop 사용
+        messagesEndRef.current.parentNode.scrollTop = messagesEndRef.current.offsetTop;
+
     }
     useEffect(() => {
-        //대화 내용이 3개를 넘어가면 자동으로 맨 밑 스크롤
-        if (dialog.length > 4) {
+        if (dialog.length>4){
             scrollToBottom();
         }
 
     }, [dialog]);
 
-    const sendMessage=(e)=>{
+    const sendMessage = (e) => {
         e.preventDefault();
-        
+
         if (!inputRef.current.value) return;
 
         //유저 메시지 등록 액션 호출
         dispatch(recordUserMsg(inputRef.current.value));
 
         //메시지 전송 및 봇 응답 수신 액션 호출
-        sendAndReceive(inputRef.current.value).then(res=>dispatch(res));
+        sendAndReceive(inputRef.current.value).then(res => {
+            dispatch(res);
+            scrollToBottom();
+        });
 
-        inputRef.current.value=''
-            
+        inputRef.current.value = ''
     }
 
     return (
         <div className='flex-col border-l-2 border-r-2 border-gray-300 md:mx-14'>
             {/* 봇과 사용자의 대화 부분 */}
             <div className='flex-col h-screen overflow-y-auto md:pt-8 pt-4  pb-32 text-xs md:text-base'
-            style={{scrollBehavior:'smooth'}}>
+                style={{ scrollBehavior: 'smooth' }}>
                 {dialog.map(message => (
                     <div key={Math.random()}>
                         <MessageObj type={message.type} msg={message.message} data={message.data} />
--- frontend/src/components/MessageObj.js
+++ frontend/src/components/MessageObj.js
@@ -1,9 +1,12 @@
 import WelfareCard from "./WelfareCard";
+import Carousel from 'nuka-carousel';
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/solid'
 
 function MessageObj({type, msg, data}) {
+
     let justify='';
     if (type==='bot' || type==='data'){
-        justify='flex justify-start mx-3 md:mx-4 md:w-5/12 w-4/5';
+        justify='flex justify-start mx-3  md:mx-4 md:w-5/12 w-4/5';
     }
     else{
         justify='flex mx-2 md:mx-4 justify-end';
@@ -14,11 +17,12 @@ function MessageObj({type, msg, data}) {
             {type==='bot' && (<div className='flex items-center md:mb-8 mb-4'>
                 {/* 봇 이미지 */}
                 <div className='flex-shrink-0'>
-                    <img src='/images/bot.png' className='md:w-12 md:h-12 w-8 h-8' />    
+                    <img src='/images/bot2.png' className='md:w-12 md:h-12 w-8 h-8' />    
                 </div>
 
                 {/* 봇 메시지 */} 
-                <div className=' flex bg-gray-100 rounded-xl ml-2 p-4 hover:scale-105 transition duration-700 transform ease-in-out '>
+                <div className=' flex bg-gray-100 rounded-xl ml-2 p-4 hover:scale-105 
+                transition duration-700 transform ease-in-out shadow-md'>
                     <p>{msg}</p>
                 </div>    
             
@@ -26,17 +30,33 @@ function MessageObj({type, msg, data}) {
         
             {type==='user' && (<div className='flex justify-end items-center md:mb-8 mb-4 md:w-5/12'>
                 {/*사용자 메시지 */}
-                <div className=' flex bg-blue-400 rounded-xl ml-2 p-4 text-white'>
+                <div className=' flex bg-blue-400 rounded-xl ml-2 p-4 text-white shadow-md'>
                     <p>{msg}</p>
                 </div>  
             </div>)}
             
             {data.length>0 && (
-                <div className='flex-col w-full'>
+                <Carousel className='w-full px-4 py-4 md:px-7 md:ml-2 md:py-8 bg-blue-50 hover:scale-105 rounded-xl 
+                hover:bg-blue-100 transform duration-500 shadow-lg md:mb-10' wrapAround={true}
+                defaultControlsConfig={{
+                    pagingDotsStyle: {
+                      fill: "green",
+                      margin:'3px'
+                    }
+                  }}
+                renderCenterLeftControls={({previousSlide})=>(
+                    <ChevronLeftIcon className='w-8 h-8 opacity-50 hover:text-gray-500 animate-bounce text-gray-400' onClick={previousSlide} />
+                )}
+                renderCenterRightControls={({nextSlide})=>(
+                    <ChevronRightIcon className='w-8 h-8 opacity-50 hover:text-gray-500 animate-bounce text-gray-400' onClick={nextSlide} />
+                )}>
                     {data.map(d=>(
-                        <WelfareCard key={d.title} title={d.title} desc={d.desc} />
+                         <div className='flex-col items-center justify-center' key={d.title}>
+                             <p className='text-blue-700 text-sm md:text-lg md:mb-4 mb-2'>{d.title}</p>
+                             <p className='md:text-sm md:pb-0 pb-2'>{d.desc}</p>
+                         </div>
                     ))}
-                </div>
+                </Carousel>
             )}
         </div>
     )
