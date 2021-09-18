de9bb8fcd5231429159e2280cc6f5be2b95012fe
유수현 <rhan0871@naver.com>
2021-09-19 02:04:08 +0900

    복지 모델 작성

--- backend/jangoback/chatbot/models.py
+++ backend/jangoback/chatbot/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Welfare(models.Model):
+    domain=models.CharField(max_length=15)
+    location=models.CharField(max_length=30)
+    location_detail=models.CharField(max_length=30)
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    link=models.TextField()
