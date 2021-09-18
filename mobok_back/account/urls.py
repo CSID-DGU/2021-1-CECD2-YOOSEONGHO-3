40054c33a705ca5096f01670220b61e31ac2accf
유수현 <rhan0871@naver.com>
2021-09-18 23:20:21 +0900

    -

--- mobok_back/account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('create/', views.createUser),
-    path('login/',views.login),
-]
--- mobok_back/welfare/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Welfare
-
-# Register your models here.
-class WelfareAdmin(admin.ModelAdmin):
-    list_display=('domain','title','description','link')
-
-admin.site.register(Welfare,WelfareAdmin)
